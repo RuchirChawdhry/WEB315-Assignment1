9bceecfc0d6a85a836cb1a9623803e92d0b91ef9
Ruchir Chawdhry <W0786978@myscc.ca>
2023-06-23 16:32:18 -0400

    Added migrations
    Added migrations for new model properties

---
+++ LibraryManagementSystem/Migrations/20230623203100_newModelProperties.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryManagementSystem.Migrations
+{
+    public partial class newModelProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Library",
+                table: "Library");
+
+            migrationBuilder.RenameTable(
+                name: "Library",
+                newName: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalPages",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPages",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "Library");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Library",
+                table: "Library",
+                column: "ID");
+        }
+    }
+}
