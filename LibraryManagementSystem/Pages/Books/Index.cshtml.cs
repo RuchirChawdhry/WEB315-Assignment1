bd23d097a1c22b13ca9b0c507ace2efa59b2af7e
Ruchir Chawdhry <W0786978@myscc.ca>
2023-06-23 17:07:39 -0400

    Search functionality
    Added search functionality, including a drop-down using SelectList

--- LibraryManagementSystem/Pages/Books/Index.cshtml.cs
+++ LibraryManagementSystem/Pages/Books/Index.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesLibrary.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace LibraryManagementSystem.Pages_Books
 {
@@ -19,10 +21,41 @@ namespace LibraryManagementSystem.Pages_Books
         }
 
         public IList<Library> Library { get;set; }
+        // search
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        //selectList STEP
+        public SelectList BookCategory { get; set; } // uses "using Microsoft.AspNetCore.Mvc.Rendering;"
+        [BindProperty(SupportsGet = true)]
+        public string Category { get; set; }
 
         public async Task OnGetAsync()
         {
-            Library = await _context.Library.ToListAsync();
+            // SELECT LIST SEARCH
+            // DROP DOWN
+            // BOOK CATEGORY
+            IQueryable<string> genreQuery = from m in _context.Library
+                                    orderby m.BookCategory
+                                    select m.BookCategory;
+
+
+            var libraries = from m in _context.Library
+                        select m;
+
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                libraries = libraries.Where(s => s.BookName.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(Category))
+            {
+                libraries = libraries.Where(x => x.BookCategory == Category);
+            }
+
+            BookCategory = new SelectList(await genreQuery.Distinct().ToListAsync());
+
+            Library = await libraries.ToListAsync();
         }
     }
 }
